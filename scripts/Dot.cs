671f97cc5711846611f2db7d9f535b0cf82ab049
amasirat <amir.basirat.2004@gmail.com>
2024-06-26 23:59:50 +0330

    added bounce to dot

--- scripts/Dot.cs
+++ scripts/Dot.cs
@@ -1,10 +1,13 @@
 using Godot;
-using System;
 
 public partial class Dot : RigidBody2D
 {
-    public override void _Ready()
+    public override void _IntegrateForces(PhysicsDirectBodyState2D state)
     {
-        
+        var collisionInfo = MoveAndCollide(LinearVelocity);
+        if(collisionInfo != null)
+        {
+            state.LinearVelocity = LinearVelocity.Bounce(collisionInfo.GetNormal());
+        }
     }
 }
