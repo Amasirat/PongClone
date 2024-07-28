f45ed6e5928edd34ee3d5cd3d15c7c0681d9e4b3
amasirat <amir.basirat.2004@gmail.com>
2024-07-28 12:17:19 +0330

    added comments to Dot.cs

--- scripts/Dot.cs
+++ scripts/Dot.cs
@@ -4,7 +4,9 @@ public partial class Dot : RigidBody2D
 {
     public override void _IntegrateForces(PhysicsDirectBodyState2D state)
     {
+        // Move the dot
         var collisionInfo = MoveAndCollide(LinearVelocity);
+        // Check for collisions and bounce the dot
         if(collisionInfo != null)
         {
             state.LinearVelocity = LinearVelocity.Bounce(collisionInfo.GetNormal());
