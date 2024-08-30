6a2a4265e2762c135d8ac39040b86572ff303c6b
amasirat <amir.basirat.2004@gmail.com>
2024-08-30 14:08:12 +0330

    made conditional sound player for Dot

--- scripts/Dot.cs
+++ scripts/Dot.cs
@@ -5,51 +5,60 @@ public partial class Dot : RigidBody2D
     public override void _Ready()
     {
         soundPlayer = GetNode<AudioStreamPlayer2D>("SoundEffect");
+        LinearVelocity = InitialVelocity;
     }
     
     public override void _IntegrateForces(PhysicsDirectBodyState2D state)
     {
-        GD.Print($"Force: ");
         // Move the dot
         var collisionInfo = MoveAndCollide(LinearVelocity);
         // Check for collisions and bounce the dot
         if(collisionInfo != null)
         {
-            state.LinearVelocity = LinearVelocity.Bounce(collisionInfo.GetNormal());
+            LinearVelocity = LinearVelocity.Bounce(collisionInfo.GetNormal());
             // Play sound upon collision
-            var rng = new RandomNumberGenerator();
-            soundPlayer.PitchScale = rng.RandfRange(0.5f, 1.0f);
-            soundPlayer.Play();
-            
+            PlaySound();
+            // Speed up Dot on Guard collision
             if (collisionInfo.GetCollider().GetType().ToString() == "Guard")
             {
-                ApplyForce(GetBoosted(state.LinearVelocity));
+                ApplyForce(GetBoostedForce(LinearVelocity));
             }
         }
-        GD.Print(state.LinearVelocity);
-    }
-
-    private Vector2 GetBoosted(Vector2 velocity)
-    {
-        Vector2 force = velocity.Normalized();
-        float boost = 0.05f;
-        if (velocity.X < 0.0f)
-        {
-            force.X -= boost;
-        }
-        else
-        {
-            force.X += boost;
-        }
-
-        return force;
     }
 
     public void Respawn(Vector2 pos)
     {
-        LinearVelocity = new Vector2(6, 4);
+        LinearVelocity = InitialVelocity;
         Position = pos;
     }
+    // Get applicable force from the given velocity. Used to give to ApplyForce to Speed up dot.
+    private Vector2 GetBoostedForce(Vector2 velocity)
+    {
+        Vector2 force = velocity.Normalized();
+        // Whatever the direction of Velocity, force must be in that direction to boost Dot
+        if (velocity.X < 0.0f)
+        {
+            force.X -= BoostAmount;
+        }
+        else
+        {
+            force.X += BoostAmount;
+        }
+        return force;
+    }
+
+    private void PlaySound()
+    {
+        if (Sound)
+        {
+            var rng = new RandomNumberGenerator();
+            soundPlayer.PitchScale = rng.RandfRange(0.5f, 1.0f);
+            soundPlayer.Play();
+        }
+    }
+    [Export] public Vector2 InitialVelocity { get; set; }
+    [Export] public float BoostAmount { get; set; }
+    [Export] public bool Sound { get; set; } = true;
     
     private AudioStreamPlayer2D soundPlayer;
 }
