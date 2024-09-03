46f1cc831075de871f8bec02b9aa985b476292a8
amasirat <amir.basirat.2004@gmail.com>
2024-09-03 15:34:28 +0330

    made functional exchange movement frontend

--- scripts/SettingsMenu.cs
+++ scripts/SettingsMenu.cs
@@ -1,7 +1,13 @@
 using Godot;
+using System.Collections.Generic;
 
 public partial class SettingsMenu : Control
 {
+    public override void _Ready()
+    {
+        
+    }
+    
     public void OnBackButtonPressed()
     {
         GetTree().ChangeSceneToFile("res://scenes/main_menu.tscn");
@@ -9,7 +15,6 @@ public partial class SettingsMenu : Control
 
     public void OnApplyButtonPressed()
     {
-        
         GetTree().ChangeSceneToFile("res://scenes/main_menu.tscn");
     }
 
@@ -25,6 +30,45 @@ public partial class SettingsMenu : Control
 
     private void OnMovementExchangeToggled(bool isToggled)
     {
-        
+        if (isToggled)
+        {
+            MakeShowRightWASD();
+        }
+        else
+        {
+            MakeShowRightArrowed();
+        }
+    }
+
+    private void MakeShowRightWASD()
+    {
+        GetNode<TextureRect>("LeftUpIcon").
+            SetTexture(ResourceLoader.Load<Texture2D>(
+                "res://assets/icons/icons8-page-up-button-50.png"));
+        GetNode<TextureRect>("LeftDownIcon").
+            SetTexture(ResourceLoader.Load<Texture2D>(
+                "res://assets/icons/icons8-page-down-button-50.png"));
+        GetNode<TextureRect>("RightUpIcon").
+            SetTexture(ResourceLoader.Load<Texture2D>(
+                "res://assets/icons/icons8-w-key-50.png"));
+        GetNode<TextureRect>("RightDownIcon").
+            SetTexture(ResourceLoader.Load<Texture2D>(
+                "res://assets/icons/icons8-s-key-50.png"));
+    }
+
+    private void MakeShowRightArrowed()
+    {
+        GetNode<TextureRect>("LeftUpIcon").
+            SetTexture(ResourceLoader.Load<Texture2D>(
+                "res://assets/icons/icons8-w-key-50.png"));
+        GetNode<TextureRect>("LeftDownIcon").
+            SetTexture(ResourceLoader.Load<Texture2D>(
+                "res://assets/icons/icons8-s-key-50.png"));
+        GetNode<TextureRect>("RightUpIcon").
+            SetTexture(ResourceLoader.Load<Texture2D>(
+                "res://assets/icons/icons8-page-up-button-50.png"));
+        GetNode<TextureRect>("RightDownIcon").
+            SetTexture(ResourceLoader.Load<Texture2D>(
+                "res://assets/icons/icons8-page-down-button-50.png"));
     }
 }
