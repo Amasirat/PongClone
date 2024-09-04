88b643c87b57911594c568139541ed6463e821a8
amasirat <amir.basirat.2004@gmail.com>
2024-09-04 17:33:49 +0330

    the main scene now downloads and applies the states recorded in user config

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 public partial class Main : Node2D
 {
@@ -10,10 +12,56 @@ public partial class Main : Node2D
 
     public override void _Ready()
     {
+        InitializeReferenceNodes();
+        states = new Dictionary<string, string>();
+        DownloadConfigStates();
+        ApplyConfigStates();
+    }
+    // do all code references of main's child nodes here, meant to be called from _Ready method
+    private void InitializeReferenceNodes()
+    {
         dotPosition = GetNode<Marker2D>("DotPosition");
         delay = GetNode<Timer>("RespawnDelay");
         timer = GetNode<Timer>("Timer");
+        timerUI = GetNode<TimerUI>("TimerUI");
+        leftGuard = GetNode<Guard>("LeftGuard");
+        rightGuard = GetNode<Guard>("RightGuard");
     }
+
+    private void DownloadConfigStates()
+    {
+        var config = FileAccess.Open(configPath, FileAccess.ModeFlags.Read);
+        while (config.GetPosition() < config.GetLength())
+        {
+            var line = config.GetCsvLine();
+            switch (line[0])
+            {
+                // only these two states are relevant to the scene
+                case "time_limit":
+                    states.Add("time_limit", line[1]);
+                    break;
+                case "revert_controls":
+                    states.Add("revert_controls", line[1]);
+                    break;
+            }
+        }
+        config.Close();
+    }
+
+    private void ApplyConfigStates()
+    {
+        timerUI.EndTime = int.Parse(states["time_limit"]);
+        
+        if (bool.Parse(states["revert_controls"]))
+        {
+            leftGuard.upAction = "arrow_move_up";
+            leftGuard.downAction = "arrow_move_down";
+
+            rightGuard.upAction = "wasd_move_up";
+            rightGuard.downAction = "wasd_move_down";
+        }
+    }
+    
     // Once the dot enters GoalArea, the main script sends a signal to the ScoreUI to change its values
     private void OnGoalAreaLeft()
     {
@@ -49,9 +97,16 @@ public partial class Main : Node2D
     {
         GetTree().ReloadCurrentScene();
     }
-
+    
+    [Export] public string configPath;
+    // references to children nodes
     private Marker2D dotPosition;
     private Timer delay;
     private Timer timer;
+    private TimerUI timerUI;
     private int dotDirection;
+    private Guard leftGuard;
+    private Guard rightGuard; 
+    // holds data downloaded from user config file
+    private Dictionary<string, string> states;
 }
